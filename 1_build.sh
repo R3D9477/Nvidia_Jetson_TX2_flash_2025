ddcf9a2aeea608be056807c19f11e3855a96f8a7
R3D9477 <r3d9477@google.com>
2025-08-21 13:57:48 +0200

    fix

--- 1_build.sh
+++ 1_build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-NVIDIA_FOLDER="$(realpath $(dirname $0)/nvidia)"
+WORKDIR="$(realpath $(dirname $0))"
+NVIDIA_FOLDER="${WORKDIR}/nvidia"
 
 sudo rm -rf $NVIDIA_FOLDER
 mkdir -p $NVIDIA_FOLDER
 
 #docker container prune
 #docker system prune -a --volumes
+
+if ! [ -f "${WORKDIR}/sdkmanager.deb" ]; then
+    mv "${WORKDIR}/"sdkmanager*.deb "${WORKDIR}/sdkmanager.deb"
+fi
 
 docker build -t jetpack-sdk .
